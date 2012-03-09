073f402c54d5794ad54444937a9da8b8e685896b
Mike Clement <mike@softwareontheside.com>
2012-03-08 23:13:41 -0700

    Base Project to start from for Randori

---
+++ NumbersToLcd/TranslatorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+//  _  _     _  _  _  _  _
+//| _| _||_||_ |_   ||_||_|
+//||_  _|  | _||_|  ||_| _| 
+
+namespace NumbersToLcd
+{
+    [TestFixture]
+    public class TranslatorTests
+    {
+        [Test]
+        public void TranslateOne()
+        {
+            Translator translator = new Translator();
+            string translatedValue = translator.Translate(1);
+            Console.WriteLine(translatedValue);
+            Assert.That(translatedValue, Is.EqualTo("   \n  |\n  |"));
+        }
+    }
+
+    public class Translator
+    {
+        public string Translate(int input)
+        {
+            return "   \n  |\n  |";
+        }
+    }
+}
